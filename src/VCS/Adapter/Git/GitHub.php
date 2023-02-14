78a565de675ffe5eae68456f501ef21a5cef06cc
Khushboo Verma <43381712+vermakhushboo@users.noreply.github.com>
2023-02-14 16:33:44 +0530

    Add list repositories and get repository methods

--- src/VCS/Adapter/Git/GitHub.php
+++ src/VCS/Adapter/Git/GitHub.php
@@ -2,10 +2,15 @@
 
 namespace Utopia\VCS\Adapter\Git;
 
+use Exception;
 use Utopia\VCS\Adapter\Git;
 
 class GitHub extends Git
 {
+    /**
+     * @var HttpClient
+     */
+    protected $httpClient;
 
     /**
      * @var string
@@ -60,5 +65,86 @@ class GitHub extends Git
     public function getAccessToken(): string
     {
         return $this->accessToken;
+    }
+
+    /**
+     * Get HTTP client
+     *
+     * @return HttpClient
+     */
+    public function getHttpClient(): HttpClient
+    {
+        return $this->httpClient;
+    }
+
+    /**
+     * Set HTTP client
+     *
+     * @param HttpClient $httpClient
+     */
+    public function setHttpClient(HttpClient $httpClient)
+    {
+        $this->httpClient = $httpClient;
+    }
+
+    /**
+     * Send a request to the GitHub API
+     *
+     * @param string $method
+     * @param string $url
+     * @param array $headers
+     * @param array|null $body
+     * @return HttpClientResponse
+     * @throws Exception
+     */
+    public function request(string $method, string $url, array $headers = [], ?array $body = null): HttpClientResponse
+    {
+        if (!$this->accessToken) {
+            throw new Exception('Access token not set');
+        }
+
+        $headers['Authorization'] = 'token ' . $this->accessToken;
+
+        try {
+            $request = new HttpClientRequest($method, $url, $headers, $body);
+            $response = $this->httpClient->send($request);
+        } catch (Exception $e) {
+            throw new Exception($e->getMessage(), $e->getCode(), $e);
+        }
+
+        if ($response->getStatusCode() >= 400) {
+            throw new Exception($response->getBody(), $response->getStatusCode());
+        }
+
+        return $response;
+    }
+
+
+    /**
+     * List repositories
+     *
+     * @return array
+     * @throws Exception
+     */
+    public function listRepositories(): array
+    {
+        $response = $this->request('GET', 'https://api.github.com/user/repos');
+
+        return json_decode($response->getBody(), true);
+    }
+
+    /**
+     * Get repository
+     *
+     * @param string $owner
+     * @param string $repo
+     * @return array
+     * @throws Exception
+     */
+    public function getRepository(string $owner, string $repo): array
+    {
+        $response = $this->request('GET', 'https://api.github.com/repos/' . urlencode($owner) . '/' . urlencode($repo));
+
+        return json_decode($response->getBody(), true);
     }
 }

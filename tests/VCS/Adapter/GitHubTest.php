2ff0a588e6e7a2e13cddef82751426fee6d08882
loks0n <22452787+loks0n@users.noreply.github.com>
2024-12-06 13:15:55 +0000

    chore: revert shallow cloning

--- tests/VCS/Adapter/GitHubTest.php
+++ tests/VCS/Adapter/GitHubTest.php
@@ -245,8 +245,6 @@ class GitHubTest extends Base
         $this->assertNotEmpty($gitCloneCommand);
         $this->assertStringContainsString('sparse-checkout', $gitCloneCommand);
 
-        fwrite(STDOUT, 'Executing command: ' . $gitCloneCommand . PHP_EOL);
-
         $output = '';
         $resultCode = null;
         \exec($gitCloneCommand, $output, $resultCode);
@@ -274,8 +272,6 @@ class GitHubTest extends Base
         $gitCloneCommand = $this->vcsAdapter->generateCloneCommand('test-kh', 'test2', '0.1.0', GitHub::CLONE_TYPE_TAG, '/tmp/clone-tag', '*');
         $this->assertNotEmpty($gitCloneCommand);
         $this->assertStringContainsString('sparse-checkout', $gitCloneCommand);
-
-        fwrite(STDOUT, 'Executing command: ' . $gitCloneCommand . PHP_EOL);
 
         $output = '';
         $resultCode = null;

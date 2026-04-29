92d52cf3ee7a58388b714900aeff8d6950805555
Chirag Aggarwal <chiragaggarwal5k@gmail.com>
2026-04-29 15:47:12 +0530

    Remove deprecated curl_close calls

--- src/VCS/Adapter.php
+++ src/VCS/Adapter.php
@@ -408,7 +408,6 @@ abstract class Adapter
             throw new Exception(curl_error($ch) . ' with status code ' . $responseStatus, $responseStatus);
         }
 
-        curl_close($ch);
 
         $responseHeaders['status-code'] = $responseStatus;
 
